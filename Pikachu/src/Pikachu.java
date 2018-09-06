adab5133753c2cc5437d9fbdeef6aa96046d6d77
Seantejones <20JonesWilliam@bprep.org>
2018-09-06 09:08:25 -0700

    Pikachu

---
+++ Pikachu/src/Pikachu.java
@@ -0,0 +1,10 @@
+import java.sql.SQLOutput;
+
+public class Pikachu {
+    public static void main(String[] args){
+        System.out.println(" (\\__/)");
+        System.out.println(" (*^.^)");
+        System.out.println("z(_(\")(\")");
+    }
+
+}
