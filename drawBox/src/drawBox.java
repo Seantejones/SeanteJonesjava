18205c329537f18a9258c26c4b35308c61682749
Seantejones <20JonesWilliam@bprep.org>
2018-09-10 09:14:52 -0700

    drawBox

---
+++ drawBox/src/drawBox.java
@@ -0,0 +1,25 @@
+public class drawBox {
+    public static void main(String[] args){
+        System.out.println("Draw Boxes");
+        drawBox();
+        drawBox();
+        drawBox();
+        System.out.println("Complete");
+    }
+    public static void drawBox(){
+        System.out.println();
+        drawTopBottom();
+        drawSides();
+        drawSides();
+        drawTopBottom();
+
+    }
+    public static void drawTopBottom(){
+        System.out.println("+------+");
+    }
+    public static void drawSides(){
+        System.out.println("|      |");
+    }
+
+
+}
