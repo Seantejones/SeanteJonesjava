609e44fc9351aa980bd9990ddd6edc039a43ed49
Seantejones <20JonesWilliam@bprep.org>
2018-10-01 09:18:00 -0700

    Rocketship

---
+++ RocketShip/src/RocketShip.java
@@ -0,0 +1,36 @@
+public class RocketShip {
+    public static final double CONSTANT = 30;
+    public static void main(String[] args) {
+        drawCone();
+        drawLine();
+
+
+    }
+    // Generate the cone for the rocket
+    public static void drawCone() {
+        for(int i = 0; i < CONSTANT/3; i*3 {
+            j--
+                    System.out.print("")
+
+        }
+
+
+    }
+    // Generate the top part of the rocket
+    public static void drawTop() {
+
+    }
+    // Generate the bottom part of the rocket
+    public static void drawBottom(){
+
+    }
+    // Generate the line between parts
+    public static void drawLine() {
+        System.out.print("+");
+        for (int j = 0; j < CONSTANT/5; j++) {
+            System.out.print("=*");
+            System.out.print("+");
+        }
+    }
+
+}
