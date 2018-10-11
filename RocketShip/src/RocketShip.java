4a49cd5709fc2f6cbc63091054c74dd78f05382e
Seantejones <20JonesWilliam@bprep.org>
2018-10-11 15:58:00 -0700

    Rocketship

--- RocketShip/src/RocketShip.java
+++ RocketShip/src/RocketShip.java
@@ -1,5 +1,5 @@
 public class RocketShip {
-    public static final int height = 13;
+    public static final int HEIGHT = 2;
 
     public static void main(String[] args) {
         drawCone();
@@ -15,8 +15,8 @@ public class RocketShip {
 
     // Generate the cone for the rocket
     public static void drawCone() {
-        for(int i = 1; i < 6; i++) { //6
-            for (int j = 6; j > i; j--) {
+        for(int i = 1; i < HEIGHT*2; i++) { //6
+            for (int j = HEIGHT*2; j > i; j--) {
                 System.out.print(" ");
             }
 
@@ -27,7 +27,7 @@ public class RocketShip {
                 for (int l = 0; l < i; l++) {
                     System.out.print("\\");
                 }
-                for (int g = height; g > i; g--) {
+                for (int g = HEIGHT*2; g > i; g--) {
                     System.out.print(" ");
                 } System.out.println();
         }
@@ -35,18 +35,18 @@ public class RocketShip {
 
     // Generate the top part of the rocket
     public static void drawTop() {
-        for(int i = 2; i >= 0; i--) { //2
+        for(int i = HEIGHT-1; i >= 0; i--) { //2
             System.out.print("|");
             for(int j = 0; j < i; j++) {
                 System.out.print(".");
             }
-            for(int k = 2; k >= i; k--) {
+            for(int k = HEIGHT-1; k >= i; k--) {
                 System.out.print("/\\");
             }
             for(int l = 0; l < i; l++) {
                         System.out.print("..");
             }
-            for (int m = 2; m >= i; m--) {
+            for (int m = HEIGHT-1; m >= i; m--) {
                 System.out.print("/\\");
             }
             for (int j = 0; j < i; j++) {
@@ -60,22 +60,22 @@ public class RocketShip {
 
     // Generate the bottom part of the rocket
     public static void drawBottom(){
-        for (int i = 2; i >= 0; i--) { //2
+        for (int i = HEIGHT-1; i >= 0; i--) { //2
             System.out.print("|");
-            for (int k = 2; k > i; k--) {
+            for (int k = HEIGHT-1; k > i; k--) {
                 System.out.print(".");
 
             }
             for (int j = 0; j <= i; j++) {
                 System.out.print("\\/");
             }
-            for (int k = 2; k > i; k--){
+            for (int k = HEIGHT-1; k > i; k--){
                 System.out.print("..");
             }
             for (int j = 0; j <= i; j++) {
                 System.out.print("\\/");
             }
-            for (int k = 2; k > i; k--) {
+            for (int k = HEIGHT-1; k > i; k--) {
                 System.out.print(".");
             }
             System.out.print("|");
@@ -91,7 +91,7 @@ public class RocketShip {
     // Generate the line between parts
     public static void drawLine() {
         System.out.print("+");
-        for (int j = 0; j < 6; j++) { //6
+        for (int j = 0; j < HEIGHT*2; j++) { //6
             System.out.print("=*");
 
         } System.out.print("+");
