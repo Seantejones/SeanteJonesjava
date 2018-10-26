b919850affdfe3a2a71ca3e7decbcd9c0e530c58
Seantejones <20JonesWilliam@bprep.org>
2018-10-26 08:34:20 -0700

    Distance Formula Project

--- Distance Project/src/Distance.java
+++ Distance Project/src/Distance.java
@@ -4,18 +4,19 @@ Period 1
 October 18, 2018
 Distance Formula Project
  */
-public class Distance {
-    public static void main(String [] args) {
-        double answer1 = distance(16, 10, 4, 28);
-        double answer2 = answer1 + distance(4, 28, 2, 8);
-        double answer3 = answer1 + answer2;
 
-        System.out.println("The distance from Tacoma to Gig Harbor is " + answer1 + " miles.");
-        System.out.println("The distance from Tacoma to Gig Harbor to Steilacoom is " + answer2 + " miles.");
+public class Distance {
+    public static void main(String[] args) {
+        double answer1 = distance(16, 10, 4, 28); //Distance from A to B
+        double answer2 = answer1 + distance(4, 28, 2, 8); //Distance from A to B to C
+        double answer3 = answer1 + answer2 + distance(2, 8, 16, 10); //Distance from A to B to C to A
+
+        System.out.println("The distance from Tacoma to Gig Harbor is " + answer1 + " miles."); 
+        System.out.println("The distance from Tacoma to Gig Harbor to Steilacoom is " + answer2 + " miles."); //Prin
         System.out.println("The distance from Tacoma to Gig Harbor to Steilacoom back to Tacoma is " + answer3 + " miles.");
     }
 
-        public static double distance (double x1, double y1, double x2, double y2){ // Create the Distance Formula
+    public static double distance(double x1, double y1, double x2, double y2) { // Create the Distance Formula
         double d1 = Math.pow(x2 - x1, 2);
         double d2 = Math.pow(y2 - y1, 2);
         double d = Math.sqrt(d1 + d2);
@@ -23,3 +24,5 @@ public class Distance {
         return d;
     }
 }
+
+
